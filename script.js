1610d48703f76c3096a37da919f7f9deb09a8bdd
MReiss97 <maxreiss21@gmail.com>
2023-12-05 13:47:59 -0500

    Added more things

--- script.js
+++ script.js
@@ -46,3 +46,42 @@ function showManalapanContent () {
   document.getElementById("contactManalapanContent").style.display = "block"; // Opens up the tab when clicked
   document.getElementById("contactManalapanTab").className = "accordionPanelTab highlight"; // Highlights the tab when clicked
 }
+
+$(document).ready(function(){
+    
+  $('.items').slick({
+dots: true,
+infinite: true,
+speed: 800,
+autoplay: true,
+autoplaySpeed: 2000,
+slidesToShow: 4,
+slidesToScroll: 4,
+responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+
+]
+});
+        });
